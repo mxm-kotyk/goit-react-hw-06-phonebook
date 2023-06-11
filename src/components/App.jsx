9c7405ebdbb354170bc6be16a04429bbd6c0e2d5
Max Kotyk <mxm.kotyk@gmail.com>
2023-06-11 11:42:48 +0100

    Removed unused code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,34 +6,6 @@ import { ContactList } from './ContactList';
 import { Wrapper, MainTitle, SecondaryTitle } from './App.styled';
 
 export const App = () => {
-  // const [contacts, setContacts] = useState(
-  //   JSON.parse(localStorage.getItem('contacts')) ?? []
-  // );
-  // const [filter, setFilter] = useState('');
-
-  // useEffect(() => {
-  //   localStorage.setItem('contacts', JSON.stringify(contacts));
-  // }, [contacts]);
-
-  // const handleInput = ({ currentTarget: { value } }) => setFilter(value);
-
-  // const addContact = (name, number) => {
-  //   setContacts(prev => [...prev, { id: `${uniqid()}`, name, number }]);
-  // };
-
-  // const filterContacts = () => {
-  //   const normalizedFilter = filter.toLowerCase();
-  //   return contacts.filter(({ name }) =>
-  //     name.toLowerCase().includes(normalizedFilter)
-  //   );
-  // };
-
-  // const deleteContact = id => {
-  //   setContacts(prev => [...prev.filter(contact => contact.id !== id)]);
-  // };
-
-  // const filteredContacts = filterContacts();
-
   return (
     <Wrapper>
       <MainTitle>Phonebook</MainTitle>
